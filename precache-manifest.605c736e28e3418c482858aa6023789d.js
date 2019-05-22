2ae11392009f15471c0033475e10f3205ac52c5e
daliabdmalek <49783051+daliabdmalek@users.noreply.github.com>
2019-05-22 10:45:52 +0200

    Updates

---
+++ precache-manifest.605c736e28e3418c482858aa6023789d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1700aababee157dc2ec2a6c22cb6ad40",
+    "url": "/React-Movie-App/index.html"
+  },
+  {
+    "revision": "48676bf41a9998ef4014",
+    "url": "/React-Movie-App/static/css/main.42cd8048.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/React-Movie-App/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "48676bf41a9998ef4014",
+    "url": "/React-Movie-App/static/js/main.c89d0628.chunk.js"
+  },
+  {
+    "revision": "22562f8c3bec8ca4aee4",
+    "url": "/React-Movie-App/static/js/runtime~main.0de861ee.js"
+  }
+]);
